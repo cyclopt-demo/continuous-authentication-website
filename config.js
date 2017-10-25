f481a63c92596e375fa8e95c66c29fa5cf31b0a1
tasos <tasoskakour@gmail.com>
2017-10-25 16:30:33 +0300

    First commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'port': process.env.PORT || 5001,
+    'database': 'mongodb://test_tasos:abc123_test@ds127101.mlab.com:27101/keystroketest', // Contact me for database credentials
+    'secret': 'ilovevodkavodkyvodkavodkyvodkyvodkaaaaa'
+};
---
+++ src/app/_models/keystrokeprofile.ts
@@ -0,0 +1,9 @@
+export class Keystrokeprofile {
+    total_keystrokes: number;
+    max: number;
+    min: number;
+    avg: number;
+    std: number;
+    fastest_digraph: string;
+    slowest_digraph: string;
+}
